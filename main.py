0c9a6aa18f78c8f0014d18db52d171dd7b7a4b70
Saphheruru <timothyjosceff@gmail.com>
2024-09-15 23:52:59 +0800

    Added Prompt template

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 import google.generativeai as genai
 import os
 import helper_functions 
+
+
 # Load env file
 load_dotenv()
 
 genai.configure(api_key= os.getenv('GOOGLE_API_KEY'))
 
-
-# Show available models
-# for model in genai.list_models():
-#     print(model.name)
-
+# LLM Model Settings
 llm = ChatGoogleGenerativeAI(model='gemini-pro', temperature= 0.5)
-response = llm.invoke('write a paragraph about warhammer 40k')
 
-helper_functions.print_output(response.content)
+# Prompmt Template 
+ai_prompt = PromptTemplate.from_template('You are a super hero, Write a short speech about your {topic}')
+
+# Chain
+sequence = ai_prompt | llm
+
+topic = "superpowers"
+helper_functions.print_output(sequence.invoke({"topic": "saving the world"}))
